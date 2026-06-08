573309cdd17f839302229982737a97d9b6d10a72
Radprr <ignaciocervantesa@gmail.com>
2026-06-08 03:06:07 -0600

    Se elimina codigo no usado

--- app/src/main/java/com/example/appalimentos/MainActivity.kt
+++ app/src/main/java/com/example/appalimentos/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.appalimentos
 
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,18 +12,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
-import com.example.appalimentos.ui.screens.FoodScreen
 import com.example.appalimentos.ui.screens.FoodSearchScreen
 import com.example.appalimentos.ui.screens.Login
 import com.example.appalimentos.ui.screens.ProfileScreen
@@ -38,9 +31,7 @@ import com.example.appalimentos.ui.screens.Register
 import com.example.appalimentos.ui.screens.Splash
 import com.example.appalimentos.ui.screens.Welcome
 import com.example.appalimentos.ui.screens.verification
-import com.sun.mail.iap.Argument
-import okhttp3.internal.connection.RouteSelector
-import java.net.Authenticator
+
 
 //CLASE MAIN PARA COMPROBAR QUE FUNCIONE
 //CODIGO PENDIENTE DE COMENTAR
