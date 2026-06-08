001aa7d4955f2f99b77818db4af10b64f0d2c675
Radprr <ignaciocervantesa@gmail.com>
2026-06-08 02:59:50 -0600

    Se agregan rutas para la navegacion de aplicacion

--- app/src/main/java/com/example/appalimentos/MainActivity.kt
+++ app/src/main/java/com/example/appalimentos/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appalimentos
 
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
 import com.example.appalimentos.ui.screens.FoodScreen
 import com.example.appalimentos.ui.screens.FoodSearchScreen
 import com.example.appalimentos.ui.screens.Login
@@ -30,12 +32,19 @@ import com.example.appalimentos.ui.screens.ProfileScreen
 import androidx.navigation.compose.NavHost //**************  Navigation
 import androidx.navigation.compose.composable  //
 import androidx.navigation.compose.rememberNavController  //
+import androidx.navigation.navArgument
 import com.example.appalimentos.ui.navigation.Routes //***********
 import com.example.appalimentos.ui.screens.Register
+import com.example.appalimentos.ui.screens.Splash
+import com.example.appalimentos.ui.screens.Welcome
 import com.example.appalimentos.ui.screens.verification
+import com.sun.mail.iap.Argument
+import okhttp3.internal.connection.RouteSelector
+import java.net.Authenticator
 
 //CLASE MAIN PARA COMPROBAR QUE FUNCIONE
 //CODIGO PENDIENTE DE COMENTAR
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,15 +56,69 @@ class MainActivity : ComponentActivity() {
 
             NavHost(
                 navController = navController,
-                startDestination = Routes.FOODS
+                startDestination = Routes.SPLASH
             ) {
 
-                composable(Routes.FOODS) {
-                    FoodSearchScreen(navController)
+                composable(Routes.SPLASH) {
+                    Splash(navController = navController)
                 }
 
-                composable(Routes.PROFILE) {
-                    ProfileScreen(navController)
+                composable(Routes.WELCOME){
+                    Welcome(navController = navController)
+                }
+
+                composable (Routes.LOGIN){
+                    Login(
+                        onLoginSuccess = {loggedEmail ->
+                            navController.navigate(Routes.createProfileRoute(loggedEmail)){
+                                popUpTo(Routes.WELCOME){inclusive = true}
+                            }
+                        },
+                        onNavigateToRegister = {navController.navigate(Routes.REGISTER)}
+                    )
+                }
+                composable(Routes.REGISTER){
+                    Register(
+                        onRegisterSuccess = {registeredEmail ->
+                        navController.navigate(Routes.createVerificationRoute(registeredEmail))
+                                            },
+                        onNavigateToLogin = {navController.navigate(Routes.LOGIN)}
+                    )
+                }
+                composable(Routes.VERIFICATION, arguments = listOf(navArgument("email"){type =
+                    NavType.StringType}))
+                { backStackEntry ->
+                    val email = backStackEntry.arguments?.getString("email")?: ""
+                    verification(
+                        email = email,
+                        onVerificationSuccess = {
+                            navController.navigate(Routes.createProfileRoute(email)){
+                                popUpTo(0){inclusive = true}
+                            }
+                        }
+                    )
+                }
+
+                composable(route = "foods/{email}", arguments = listOf(navArgument("email"){type =
+                    NavType.StringType}))
+                {backStackEntry ->
+                    val email = backStackEntry.arguments?.getString("email") ?: ""
+                    FoodSearchScreen(navController, userEmail = email)
+                }
+                //SE MODIFICA PARA OBTENER LA INFORMACION DEL USUARIO QUE INGRESO
+                composable(Routes.PROFILE, arguments = listOf(navArgument("email") { type = NavType.StringType}))
+                {
+                    backStackEntry ->
+                    val userEmail = backStackEntry.arguments?.getString("email")?:""
+                    ProfileScreen(
+                        onLogOut = {
+                            navController.navigate(Routes.LOGIN){
+                                popUpTo(0) {inclusive = true}
+                            }
+                        },
+                        navController = navController,
+                        userEmail = userEmail
+                    )
                 }
             }
             //*****
@@ -63,7 +126,8 @@ class MainActivity : ComponentActivity() {
             //ProfileScreen()// prueba para ver si funciona
 
 
-            /* // Parte de login ignacio
+            // Parte de login ignacio
+            /*
             var currentAuthScreen by remember { mutableStateOf("login") }
             var userLoggedIn by remember { mutableStateOf(false) }
 
